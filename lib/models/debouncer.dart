6b8fdc5100fa05c1f816c34bf40bf0d1237e063e
notCactus <cactusdevcode@gmail.com>
2020-06-21 19:51:26 +0200

    Formating

--- lib/models/debouncer.dart
+++ lib/models/debouncer.dart
@@ -15,6 +15,7 @@ class Debouncer {
   final int milliseconds;
   VoidCallback action;
   Timer _timer;
+  
   Debouncer({this.milliseconds});
   run(VoidCallback action) {
     if (_timer != null) {

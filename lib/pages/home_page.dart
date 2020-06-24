2697af4e73b46cf0beccfc4b8f84bfca2deb6591
notCactus <cactusdevcode@gmail.com>
2020-06-24 19:01:06 +0200

    Remove experimental code.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,33 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-import '../utils/devfile.dart';
 import '../widgets/search.dart';
-import '../models/stations.dart';
-
-Future<Stations> fetchStations() async {
-  const String baseUrl =
-      'https://api.resrobot.se/v2/location.name?key=${ApiKey.key}';
-  String url = baseUrl + '&input=Fridhems&format=json';
-
-  Stations stationsList;
-
-  try {
-    final resp = await http.get(url);
-    if (resp.statusCode == 200) {
-      stationsList = Stations.fromJson(jsonDecode(resp.body));
-
-      return stationsList;
-    } else {
-      print('Status Code: 404');
-      return stationsList;
-    }
-  } catch (e) {
-    print(e);
-  }
-}
 
 class HomePage extends StatelessWidget {
   static const routeName = '/home';
@@ -45,10 +20,7 @@ class HomePage extends StatelessWidget {
       ),
       backgroundColor: const Color(0xFFECECEC),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          Stations stations = await fetchStations();
-          print(stations.stopLocation[0].id);
-        },
+        onPressed: () {},
         child: const Icon(Icons.star),
       ),
     );
